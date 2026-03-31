9658b380a74a07b7f284f7925dab37eb0424c5ed
lexxai <lexxai@gmail.com>
2026-03-31 03:56:59 +0300

    ## [0.2.0] - 2026-03-31
    ### Added
    - Usage a new options `BINARY_RESPONSE` from `django-mariadb-vector` v0.2.0 for optimized batch mode. [Performance results](https://lexxai.blogspot.com/2026/03/comparing-performance-after.html).
    ### Changed
    - Docker settings for build on multiple os versions, added package `build-essential`.
    - Dependency to `django-mariadb-vector[orjson]>=0.2.0`

--- src/articles/models.py
+++ src/articles/models.py
@@ -3,15 +3,30 @@ from django.db.models import TextField
 from django_mariadb_vector import MariaDBVectorField, MariaDBVectorIndex
 from django_mariadb_vector.managers import RecommendationManager
 
-from config.settings import VECTOR_EMBEDDING_DIMENSION, VECTOR_EMBEDDING_INDEX_M
+try:
+    from django_mariadb_vector import __version__ as django_mariadb_vector_version
+except ImportError:
+    __version__ = "0.0.0"
+
+from config.settings import VECTOR_EMBEDDING_DIMENSION, VECTOR_EMBEDDING_INDEX_M, BINARY_RESPONSE
 
 DIMENSIONS = VECTOR_EMBEDDING_DIMENSION
 VECTOR_INDEX_M = VECTOR_EMBEDDING_INDEX_M
+DJANGO_MARIADB_VECTOR_MIN_VERSION = (0, 2, 0)
+
+ver = tuple(map(int, django_mariadb_vector_version.split(".")[:3]))
+if ver < DJANGO_MARIADB_VECTOR_MIN_VERSION:
+    assert (
+        False
+    ), f"django-mariadb-vector version must be at least {DJANGO_MARIADB_VECTOR_MIN_VERSION}, found {django_mariadb_vector_version}"
+
+
+print(f"django_mariadb_vector_version: v{django_mariadb_vector_version}, {DIMENSIONS=}, {BINARY_RESPONSE=}")
 
 
 class Article(models.Model):
     context = TextField()
-    embedding = MariaDBVectorField(dimensions=DIMENSIONS)
+    embedding = MariaDBVectorField(dimensions=DIMENSIONS, binary_response=BINARY_RESPONSE)
 
     objects = RecommendationManager(vector_field="embedding")
 
